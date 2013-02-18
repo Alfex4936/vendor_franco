fbbc3d52c54a38dc56f1bccb151e7d46e4ddc707
franco-c <tjrdnjs33936@gmail.com>
2013-02-18 11:50:15 +0900

    No drm

--- config/common.mk
+++ config/common.mk
@@ -18,15 +18,7 @@ $(call inherit-product, frameworks/base/data/videos/VideoPackage2.mk)
 # Enable ADB authentication
 ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
 
-# Blobs necessary for drm
-PRODUCT_COPY_FILES +=  \
-   vendor/aosp/prebuilt/common/lib/libfrsdk.so:system/lib/libfrsdk.so \
-   vendor/aosp/prebuilt/common/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
-   vendor/aosp/prebuilt/common/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-   vendor/aosp/prebuilt/common/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
-   vendor/aosp/prebuilt/common/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so 
-
-# Blobs necessary for drm
+# Blobs necessary for Keyboard
 PRODUCT_COPY_FILES +=  \
    vendor/aosp/prebuilt/common/lib/libjni_koreanime.so:system/lib/libjni_koreanime.so
 
