12d88b3c328c77304af50b6ebcbdac784aaadef8
franco-c <tjrdnjs33936@gmail.com>
2013-02-23 08:34:47 +0900

    Funny ROM

--- config/franco.mk
+++ config/franco.mk
@@ -16,6 +16,7 @@ PRODUCT_PACKAGES += \
     KoreanKeyboard \
     Development
 
+# Fucking Funkies
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libjni_koreanime.so:system/lib/libjni_koreanime.so \
     vendor/aosp/prebuilt/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
@@ -31,4 +32,8 @@ PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libpatts_engine_jni_api.so:system/lib/libpatts_engine_jni_api.so \
     vendor/aosp/prebuilt/common/lib/libspeexwrapper.so:system/lib/libspeexwrapper.so \
     vendor/aosp/prebuilt/common/lib/libvorbisencoder.so:system/lib/libvorbisencoder.so
+
+# For System Information & Build zip name
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.franco.version=$(TARGET_PRODUCT)-mirage-reborn-001
     
