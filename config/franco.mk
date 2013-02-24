2a44146ea8644c89e4915afcd877a55730c83c89
franco-c <tjrdnjs33936@gmail.com>
2013-02-24 12:48:30 +0900

    Gapps possibly fix

--- config/franco.mk
+++ config/franco.mk
@@ -3,6 +3,7 @@ PRODUCT_PACKAGES += \
     ChromeBookmarksSyncAdapter \
     GoogleBackupTransport \
     GoogleContactsSyncAdapter \
+    GoogleFeedback \
     GoogleLoginService \
     GooglePartnerSetup \
     GoogleServicesFramework \
@@ -14,15 +15,18 @@ PRODUCT_PACKAGES += \
     SetupWizard \
     PerformanceControl \
     KoreanKeyboard \
-    Development
+    Development \
+    QuickSearchBox
 
 # Fucking Funkies
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/lib/libjni_koreanime.so:system/lib/libjni_koreanime.so \
+    vendor/aosp/prebuilt/common/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
     vendor/aosp/prebuilt/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml \
     vendor/aosp/prebuilt/common/etc/permissions/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml \
     vendor/aosp/prebuilt/common/etc/permissions/features.xml:system/etc/permissions/features.xml \
     vendor/aosp/prebuilt/common/etc/preferred-apps/google.xml:system/etc/preferred-apps/google.xml \
+    vendor/aosp/prebuilt/common/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar \
     vendor/aosp/prebuilt/common/framework/com.google.android.media.effects.jar:system/framework/com.google.android.media.effects.jar \
     vendor/aosp/prebuilt/common/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
     vendor/aosp/prebuilt/common/lib/libfrsdk.so:system/lib/libfrsdk.so \
